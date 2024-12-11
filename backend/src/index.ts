b18e721fd14bf1cbeb9abcb62187a57d5bd5b287
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-12-12 00:20:05 +0530

    added signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,70 +1,120 @@
 import express from "express";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
+import prisma from "./prisma";
+import zod from "zod";
+import bcrypt from "bcrypt";
+import { Keypair } from "@solana/web3.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.post("/api/v1/signup",(async(req:any,res:any)=>{
+app.post("/api/v1/signup", async (req: any, res: any) => {
+  const signupBody = zod.object({
+    username: zod.string().min(1),
+    password: zod.string().min(1),
+  });
 
-    const {username,password} = req.body;
+  const JWT_SECRET = process.env.JWT_SECRET;
 
-    try {
-       res.json("signup");
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message:"Internal server error"
-        })
+  try {
+    const { success } = signupBody.safeParse(req.body);
+
+    if (!success) {
+      return res.status(403).json({
+        message: "Invalid form of input",
+      });
     }
-}))
 
-app.post("/api/v1/signin",(async(req:any,res:any)=>{
+    const { username, password } = req.body;
 
-    const {username,password} = req.body;
-
-    try {
-       res.json("signin");
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message:"Internal server error"
-        })
+    const ExistingUser = await prisma.user.findFirst({
+      where: {
+        username: username,
+      },
+    });
+    if (ExistingUser) {
+      return res.status(411).json({
+        message: "User already exists",
+      });
     }
-}))
 
-app.post("/api/v1/txn/sign",(async(req:any,res:any)=>{
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    const {username,password} = req.body;
+    const keypair = new Keypair();
+    const publicKey = keypair.publicKey.toBase58();
+    const privateKey = Buffer.from(keypair.secretKey).toString("base64");
 
-    try {
-       res.status(200).json(
-        {
-        id: "id_to_track_txn"
-    }
-)
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message:"Internal server error"
-        })
-    }
-}))
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword,
+        privateKey: privateKey,
+        publicKey: publicKey,
+      },
+    });
 
-app.post("/api/v1/txn/?id=id",(async(req:any,res:any)=>{
+    const token = jwt.sign({ id: user.id }, JWT_SECRET || "jiojjkh");
 
-    const {username,password} = req.body;
+    return res.status(200).json({
+      message: "Signup successful",
+      token,
+      username,
+      publicKey,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
 
-    try {
-       res.status(200).json(
-        {
-            signatures: ["xyz"],
-            status: "processing"
-        }
-       );
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message:"Internal server error"
-        })
-    }
-}))
+app.post("/api/v1/signin", async (req: any, res: any) => {
+  const { username, password } = req.body;
+
+  try {
+    res.json("signin");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
+
+app.post("/api/v1/txn/sign", async (req: any, res: any) => {
+  const { username, password } = req.body;
+
+  try {
+    res.status(200).json({
+      id: "id_to_track_txn",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
+
+app.post("/api/v1/txn/?id=id", async (req: any, res: any) => {
+  const { username, password } = req.body;
+
+  try {
+    res.status(200).json({
+      signatures: ["xyz"],
+      status: "processing",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
+
+app.listen(3000,()=>{
+    console.log("http://localhost:3000");
+})
